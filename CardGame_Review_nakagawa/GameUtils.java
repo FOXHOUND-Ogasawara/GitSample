c666e5bc4a02a64d61e78c9647bbd472885e6119
Atsushi Ogasawara <atsushi.ogasawara@fox-hound.jp>
2023-02-16 16:13:04 +0900

    feat:中川さんコード追加。プリリパさんフォルダ名変更

---
+++ CardGame_Review_nakagawa/GameUtils.java
@@ -0,0 +1,52 @@
+package card;
+import java.util.Random;
+import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+
+public final class GameUtils {
+  
+  public static final String REGEX_ALPHABET = "^[A-Za-z]{4,12}$";
+
+  private GameUtils() {
+
+  }
+  public static String getInputString() {
+    String inputString = null;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    try {
+      inputString = br. readLine();
+  }catch(IOException e) {
+    System.out.println("Input error...Please retype!");
+    getInputString();
+    }
+    return inputString;
+  }
+
+  public static int getInputInt() {
+    int inputInt = 0;
+    try {
+      inputInt = Integer.parseInt(getInputString());
+    }catch(NumberFormatException e) {
+      System.out.println("Please enter an integer!");
+      getInputInt();
+    }
+    return inputInt;
+  }
+
+  public static int getRandomInt(int maxValue) {
+    Random random = new Random();
+    return random.nextInt(maxValue);
+  }
+  public static boolean checkPattern(String targetStr) {
+
+    if(targetStr == null || targetStr.isEmpty()) {
+      return false;
+    }
+    return Pattern.matches(REGEX_ALPHABET,targetStr);
+  }
+}
---
+++ CardGame_Review_nakagawa/playing.java
@@ -0,0 +1,29 @@
+package card;
+
+public class playing {
+  public static void main(String[] args) {
+    String username = "";
+    int possessionCoin = 1000;
+
+    System.out.println("Welcome !");
+
+    System.out.println("Enter your username");
+
+    
+    while(true) {
+      username = GameUtils.getInputString();
+
+      if(GameUtils.checkPattern(username)) {
+        break;
+      }
+      System.out.println("Dose not match condition of the username");
+    }
+    System.out.println("Hello" + username);
+
+    CardPickGame cardPickGame = new CardPickGame(possessionCoin);
+    possessionCoin  = cardPickGame.execute();
+
+
+    System.out.println(username + "Possession : " + possessionCoin + "Coin");
+  }
+}
