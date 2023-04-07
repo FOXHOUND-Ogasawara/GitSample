fa8d5b8d87005c5a503aaefafca89afb27f3f158
Atsushi Ogasawara <atsushi.ogasawara@fox-hound.jp>
2023-04-07 13:25:01 +0900

    no message

--- skillTest/skillTest_prylypa/javaAnswer01/SkillTest.java
+++
@@ -1,8 +0,0 @@
-class SkillTest {
-    public static void main (String[] args){
-        //問.01 回答の始まり-------------------------
-        Animal animal = new Animal("パンダ", 12);
-        animal.showProperties();
-        //問.01 回答の`終わり-------------------------
-    }
-}
