f8431fb7392ee3464f66295ce2410e0e3394c996
FOXHOUND-Ogasawara <124853788+FOXHOUND-Ogasawara@users.noreply.github.com>
2024-01-16 15:25:35 +0900

    差分コミット

---
+++ BubbleSortAlgorithm.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSortAlgorithm {
+    public static void main(String[] args) {
+        // ステップ1: 配列を宣言する
+        int[] array = {7, 3, 10, 5, 1};
+
+        // ステップ2: 2重ループを使用する
+        for (int outer = 0; outer < array.length - 1; outer++) {
+            for (int i = array.length - 1; i > outer; i--) {
+                // ステップ3: 内側のループの処理
+                if (array[i] < array[i - 1]) {
+                    // ステップ3-1: 比較してarray[i]がarray[i - 1]より小さい場合
+                    // ステップ3-2: tmpにarray[i]を代入
+                    int tmp = array[i];
+                    // ステップ3-3: array[i]にarray[i - 1]を代入
+                    array[i] = array[i - 1];
+                    // ステップ3-4: array[i - 1]にtmpを代入
+                    array[i - 1] = tmp;
+                }
+            }
+        }
+
+        // ステップ4: ソート後の配列を出力する
+        System.out.println(Arrays.toString(array));
+    }
+}
